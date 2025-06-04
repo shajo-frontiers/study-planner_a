457a62e95b7b912bef9d775aea062d9358334d9c
jingisukann <a23.hpdj@g.chuo-u.ac.jp>
2025-06-04 14:37:04 +0900

    Descriptionコンポーネントを定義

---
+++ src/Description.tsx
@@ -0,0 +1,13 @@
+
+function Description(){
+
+    return (
+        <section>
+          <p><strong>時間割を計画するアプリです。</strong><a>詳しく</a></p>
+          <p>中央大学生向けです。</p>
+         </section>
+    );
+    
+}
+
+export default Description;
